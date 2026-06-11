4985f166da202559f4691a41100a86ba0eba8b3b
manish pandey <mpandey1894@gmail.com>
2026-06-11 10:32:27 +0530

    Gym website development version 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { title } from 'node:process';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('demo-website');
-}
+  projectTitle= 'Angular learning';
+  gymServices=[
+    {
+    title:'weight lifting',
+    description:'apna stamina aur badhao',
+    icon:'fitness_center'
+    },
+    {
+    title:'Cardio Training',
+    description:'apna stamina aur badhao',
+    icon:'directions_run'
+    },
+    {
+    title:'Yoga & Flexibility',
+    description:'Mansik shanti ke liye',
+    icon:'self_improvement'
+    }
+  ]
+  }
+
